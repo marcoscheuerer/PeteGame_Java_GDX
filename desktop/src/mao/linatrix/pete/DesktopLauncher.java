0d4c3009353e1665c5e7ff8c5bc0a6f0928e6b06
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-06 16:34:49 +0200

    Starting petegame-project by an empty set

---
+++ desktop/src/mao/linatrix/pete/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package mao.linatrix.pete;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import mao.linatrix.pete.PeteGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PeteGame");
+		new Lwjgl3Application(new PeteGame(), config);
+	}
+}
